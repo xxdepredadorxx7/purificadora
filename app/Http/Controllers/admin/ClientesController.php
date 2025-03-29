bf35f1ad68cb946b1f36fea48271bd04ee8a1d3d
Ulises Cigarroa <xxdepredadorxx7@gmail.com>
2025-03-29 17:45:08 -0600

    Añadir listado de clientes en la vista de administración

--- app/Http/Controllers/admin/ClientesController.php
+++ app/Http/Controllers/admin/ClientesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
     public function index(){
-        return view('admin.clientes.index');
+        $clientes = User::where('role', 'cliente')->get();
+        return view('admin.clientes.index', compact('clientes'));
     }
 }
