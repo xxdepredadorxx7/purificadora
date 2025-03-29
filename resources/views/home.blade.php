e7231690fa7fed14cf3a0d4c50aa1c2b3af15658
Ulises Cigarroa <xxdepredadorxx7@gmail.com>
2025-03-29 17:16:47 -0600

    Añadir manejo de errores en la vista Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,12 @@
 @stop
 
 @section('content')
+
+@if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
     <p>Bienvenido a la vista Home</p>
 @stop
 
