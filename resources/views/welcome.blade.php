227727faf67dc3c38ad127046241af4f028d8c07
Ulises Cigarroa <xxdepredadorxx7@gmail.com>
2025-03-29 13:40:13 -0600

    Creacion de las Rutas, Controlador y Vistas Admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,20 @@
-Hola
+@extends('adminlte::page')
+
+@section('title', 'Bienvenida')
+
+@section('content_header')
+    <h1>Bienvenida</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido a la vista Bienvenida</p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
