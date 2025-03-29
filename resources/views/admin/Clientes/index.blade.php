5052255eb133cf111ff4d46a209514016a91d89b
Ulises Cigarroa <xxdepredadorxx7@gmail.com>
2025-03-29 17:05:13 -0600

    Añadir vista de administración para la gestión de usuarios

---
+++ resources/views/admin/Clientes/index.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page')
+
+@section('title', 'Administrador')
+
+@section('content_header')
+    <h1>Bienvenido</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido a la vista Usuarios</p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
+
+
