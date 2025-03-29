e126120703a8151e99e0f27e789c7b0178c55d87
Ulises Cigarroa <xxdepredadorxx7@gmail.com>
2025-03-29 17:05:45 -0600

    Añadir vista de administración para la gestión de productos

---
+++ resources/views/Productos/index.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Administrador')
+
+@section('content_header')
+    <h1>Bienvenido</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido a la vista Productos para usuarios</p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
+
