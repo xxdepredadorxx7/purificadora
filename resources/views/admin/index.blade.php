0af92563d188021469f9b048411337c829f130f6
Ulises Cigarroa <xxdepredadorxx7@gmail.com>
2025-03-30 09:55:14 -0600

    Añadir estadísticas de clientes, pedidos, productos e inventario en el panel de administración

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,15 +7,108 @@
 @stop
 
 @section('content')
-    <p>Bienvenido a la vista Panel del Administrador</p>
+    {{-- Estadísticas --}}
+    <div class="row">
+        {{-- Clientes Registrados --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $clientesCount }}</h3>
+                    <p>Clientes Registrados</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-users"></i>
+                </div>
+                <a href="{{ route('admin.clientes.index') }}" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        {{-- Pedidos Realizados --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $pedidosCount }}</h3>
+                    <p>Pedidos Realizados</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-shopping-cart"></i>
+                </div>
+                <a href="{{ route('admin.pedidos.index') }}" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        {{-- Productos Disponibles --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $productosCount }}</h3>
+                    <p>Productos Disponibles</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-boxes"></i>
+                </div>
+                <a href="{{ route('admin.productos.index') }}" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        {{-- Inventario Total --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $inventarioCount }}</h3>
+                    <p>Inventario Total</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-warehouse"></i>
+                </div>
+                <a href="{{ route('admin.inventario.index') }}" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
+
+    {{-- Últimos Clientes Registrados --}}
+    <div class="card mt-4">
+        <div class="card-header">
+            <h3 class="card-title">Últimos Clientes Registrados</h3>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>Fecha de Registro</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($ultimosClientes as $cliente)
+                        <tr>
+                            <td>{{ $cliente->id }}</td>
+                            <td>{{ $cliente->name }}</td>
+                            <td>{{ $cliente->email }}</td>
+                            <td>{{ $cliente->created_at->format('d/m/Y') }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="4" class="text-center">No hay clientes registrados recientemente.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
 @stop
 
 @section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+    {{-- Estilos personalizados --}}
+    <style>
+        .small-box {
+            border-radius: 10px;
+        }
+    </style>
 @stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <script> console.log("Vista del administrador cargada correctamente."); </script>
 @stop
-
