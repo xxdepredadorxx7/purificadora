778e4b4056cbd98c825798833b45b81b4a819404
Ulises Cigarroa <xxdepredadorxx7@gmail.com>
2025-03-30 16:00:49 -0600

    Añadir títulos y encabezados de contenido a las vistas de edición y perfil del usuario autenticado

--- resources/views/Perfil/index.blade.php
+++ resources/views/Perfil/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('title', 'Perfil')
+
+@section('content_header')
+    <h1>{{ Auth::user()->name }}</h1>
+@stop
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
