fd2bace9c5c75493d0b85836dd4e3bdf273413f2
Ulises Cigarroa Aguilar <54188947+xxdepredadorxx7@users.noreply.github.com>
2025-04-23 13:25:49 -0600

    Actualizar HomeAdminController: contar productos e inventarios en el método index

--- app/Http/Controllers/admin/HomeAdminController.php
+++ app/Http/Controllers/admin/HomeAdminController.php
@@ -6,14 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Pedido;
+use App\Models\Producto;
+use App\Models\Inventario;
 
 class HomeAdminController extends Controller
 {
     public function index(){
         $clientesCount = User::where('role', 'cliente')->count();
         $pedidosCount = 0; // Pedido::count();
-        $productosCount = 0; // Producto::count();
-        $inventarioCount = 0; // Inventario::count();
+        $productosCount = Producto::count();
+        $inventarioCount = Inventario::count();
         $ultimosClientes = User::where('role', 'cliente')->latest()->take(5)->get();
 
         return view('admin.index', compact('clientesCount', 'pedidosCount','productosCount','inventarioCount', 'ultimosClientes'));
