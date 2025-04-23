c475cd78f886787de72596960f0292da34713620
Ulises Cigarroa Aguilar <54188947+xxdepredadorxx7@users.noreply.github.com>
2025-04-23 11:00:18 -0600

    Actualizar vista de bienvenida: cambiar texto y icono de puntos de distribución a sucursal activa, ajustar diseño de servicios y modificar imágenes de productos.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,10 +49,10 @@
                 <div class="small-box bg-warning">
                     <div class="inner">
                         <h3>1</h3>
-                        <p>Puntos de Distribución</p>
+                        <p>Sucursal Activa</p>
                     </div>
                     <div class="icon">
-                        <i class="fas fa-map-marker-alt"></i>
+                        <i class="fas fa-store"></i>
                     </div>
                     <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
@@ -78,8 +78,8 @@
         <!-- Main row -->
         <div class="row">
             <!-- Left col -->
-            <section class="col-lg-7 connectedSortable">
-                <!-- Custom tabs (Charts with tabs)-->
+            <section class="col-lg-12 connectedSortable">
+                <!-- Nuestros Servicios -->
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">
@@ -88,42 +88,22 @@
                         </h3>
                     </div><!-- /.card-header -->
                     <div class="card-body">
-                        <div class="tab-content p-0">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="info-box mb-3 bg-gradient-info">
-                                        <span class="info-box-icon"><i class="fas fa-water"></i></span>
-                                        <div class="info-box-content">
-                                            <span class="info-box-text">Purificación</span>
-                                            <span class="info-box-number">Proceso de 5 etapas</span>
-                                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="info-box mb-3 bg-gradient-info">
+                                    <span class="info-box-icon"><i class="fas fa-water"></i></span>
+                                    <div class="info-box-content">
+                                        <span class="info-box-text">Purificación</span>
+                                        <span class="info-box-number">Proceso de 5 etapas</span>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="info-box mb-3 bg-gradient-success">
-                                        <span class="info-box-icon"><i class="fas fa-truck"></i></span>
-                                        <div class="info-box-content">
-                                            <span class="info-box-text">Entrega a Domicilio</span>
-                                            <span class="info-box-number">Servicio Express</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="info-box mb-3 bg-gradient-warning">
-                                        <span class="info-box-icon"><i class="fas fa-recycle"></i></span>
-                                        <div class="info-box-content">
-                                            <span class="info-box-text">Garrafones</span>
-                                            <span class="info-box-number">Ecológicos</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="info-box mb-3 bg-gradient-danger">
-                                        <span class="info-box-icon"><i class="fas fa-flask"></i></span>
-                                        <div class="info-box-content">
-                                            <span class="info-box-text">Análisis</span>
-                                            <span class="info-box-number">Laboratorio Certificado</span>
-                                        </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="info-box mb-3 bg-gradient-success">
+                                    <span class="info-box-icon"><i class="fas fa-truck"></i></span>
+                                    <div class="info-box-content">
+                                        <span class="info-box-text">Entrega a Domicilio</span>
+                                        <span class="info-box-number">Servicio Express</span>
                                     </div>
                                 </div>
                             </div>
@@ -142,7 +122,7 @@
                         <ul class="products-list product-list-in-card pl-2 pr-2">
                             <li class="item">
                                 <div class="product-img">
-                                    <img src="https://via.placeholder.com/50" alt="Product Image" class="img-size-50">
+                                    <img src="{{ asset('imagenes/garrafon.png') }}" alt="Garrafón 20L" class="img-size-50">
                                 </div>
                                 <div class="product-info">
                                     <a href="javascript:void(0)" class="product-title">Garrafón 20L
@@ -155,13 +135,13 @@
                             <!-- /.item -->
                             <li class="item">
                                 <div class="product-img">
-                                    <img src="https://via.placeholder.com/50" alt="Product Image" class="img-size-50">
+                                    <img src="{{ asset('imagenes/garrafon.png') }}" alt="Relleno de Garrafón" class="img-size-50">
                                 </div>
                                 <div class="product-info">
-                                    <a href="javascript:void(0)" class="product-title">Sistema de Filtrado
-                                        <span class="badge badge-danger float-right">$1,200.00</span></a>
+                                    <a href="javascript:void(0)" class="product-title">Relleno de Garrafón
+                                        <span class="badge badge-success float-right">$15.00</span></a>
                                     <span class="product-description">
-                                        Filtro doméstico para toda la casa
+                                        Servicio de relleno de garrafón con agua purificada
                                     </span>
                                 </div>
                             </li>
@@ -169,84 +149,10 @@
                         </ul>
                     </div>
                     <!-- /.card-body -->
-                    <div class="card-footer text-center">
-                        <a {{-- href="{{ route('productos.index') }}" --}} class="uppercase">Ver todos los productos</a>
-                    </div>
-                    <!-- /.card-footer -->
                 </div>
                 <!-- /.card -->
             </section>
             <!-- /.Left col -->
-
-            <!-- right col (We are only adding the ID to make the widgets sortable)-->
-            <section class="col-lg-5 connectedSortable">
-                <!-- Map card -->
-                <div class="card bg-gradient-primary">
-                    <div class="card-header border-0">
-                        <h3 class="card-title">
-                            <i class="fas fa-map-marker-alt mr-1"></i>
-                            Ubicación
-                        </h3>
-                    </div>
-                    <div class="card-body">
-                        <div id="world-map" style="height: 250px; width: 100%;"></div>
-                    </div>
-                    <div class="card-footer bg-transparent">
-                        <div class="row">
-                            <div class="col-4 text-center">
-                                <div id="sparkline-1"></div>
-                                <div class="text-white">Sucursal Centro</div>
-                            </div>
-                            <div class="col-4 text-center">
-                                <div id="sparkline-2"></div>
-                                <div class="text-white">Sucursal Norte</div>
-                            </div>
-                            <div class="col-4 text-center">
-                                <div id="sparkline-3"></div>
-                                <div class="text-white">Sucursal Sur</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- /.card -->
-
-                <!-- Calendar -->
-                <div class="card bg-gradient-success">
-                    <div class="card-header border-0">
-                        <h3 class="card-title">
-                            <i class="far fa-calendar-alt"></i>
-                            Calendario
-                        </h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body pt-0">
-                        <!--The calendar -->
-                        <div id="calendar" style="width: 100%"></div>
-                    </div>
-                    <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-
-                <!-- Promociones -->
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">
-                            <i class="fas fa-percentage"></i>
-                            Promociones
-                        </h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <div class="callout callout-info">
-                            <h5>10% de descuento</h5>
-                            <p>En tu primera recarga de garrafones</p>
-                        </div>
-                    </div>
-                    <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-            </section>
-            <!-- right col -->
         </div>
         <!-- /.row (main row) -->
     </div><!-- /.container-fluid -->
@@ -266,7 +172,6 @@
     <script src="{{ asset('vendor/adminlte/dist/js/demo.js') }}"></script>
     <script>
         $(document).ready(function() {
-            // Inicializar elementos necesarios
             console.log('Página de Purificadora de Agua cargada');
         });
     </script>
