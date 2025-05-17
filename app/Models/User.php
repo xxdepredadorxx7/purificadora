2037012fc7e324999670387760c35ffba419b42a
Ulises Cigarroa Aguilar <54188947+xxdepredadorxx7@users.noreply.github.com>
2025-05-16 23:25:13 -0600

    Corregir nombres de espacio en controladores y modelo, y actualizar la clase ProductoSeeder para seguir las convenciones de nomenclatura.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * Los atributos que se pueden asignar masivamente.
@@ -46,13 +47,11 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime', // Convierte la fecha de verificación del email a un objeto DateTime
-            'password' => 'hashed',           // Indica que la contraseña está encriptada
-        ];
-    }
+    protected $casts = [
+    'email_verified_at' => 'datetime',
+    'password' => 'hashed',
+    ];
+
 
     /**
      * Método para obtener la imagen de perfil en AdminLTE.
--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Producto;
 
-class productoSeeder extends Seeder
+class ProductoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
